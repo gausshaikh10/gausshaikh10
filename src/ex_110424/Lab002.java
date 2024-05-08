ab1ba27fb7ce15260eaa1c6a78975c6f72673b22
Mohammed Gaus <skgaus626@gmail.com>
2024-05-08 16:57:41 +0530

    My First Commit

---
+++ src/ex_110424/Lab002.java
@@ -0,0 +1,21 @@
+package ex_110424;
+/**
+ * Author Gaus
+ *program name ATB6x
+ * Lab no 2
+ * System
+ *
+ *
+ */
+
+public class Lab002 {
+    public static void main(String[] args) {
+       int age = 45;
+        System.out.println(age);
+
+
+        int a2 = 13;
+        int a3 = 14;
+        System.out.println(a2+a3);
+    }
+}
